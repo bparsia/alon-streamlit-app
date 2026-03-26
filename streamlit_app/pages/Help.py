8f5bc931dc636827d972d28461a82df76a629186
bparsia <bijan.parsia@manchester.ac.uk>
2026-03-26 12:21:19 +0000

    Testing formatting

--- streamlit_app/pages/Help.py
+++ streamlit_app/pages/Help.py
@@ -22,7 +22,7 @@ Let's suppose we are trying to capture a scenario where we have one agent, Alice
 
 We need to specify some stuff before getting to the model structure. This is done in the "header" of the diagram (using a YAML dialect):
 
-```yaml
+``` yaml
 ---
 title: Trivial 1 agent case
 type: DBT
@@ -36,6 +36,7 @@ aliases:
   ss: stands still
   q: Dan dies
 ---
+```
 
 The header is bracked by `---` each on it's own line. You can add a title. `type` is either `DBT` or `index`.
 
@@ -43,4 +44,20 @@ Agents are represented by positive integers. For each agent you need to enumerat
 
 You can also (optionally) add informative aliases for agents, action types, and propositions but currently these are just for the reader.
 
+After the closing `---`, you specify the model structure as a Mermaid class diagram:
+
+``` mermaid
+classDiagram
+direction BT
+  m --> m1 : h1({sd1})
+  m --> m2 : h2({ss1})
+  m1: q
+  m2: ~q
+```
+
+Mermaid class diagrams are very flexible. We recommend that the direction be set to `BT` (bottom to top) to reflect traditional formatting of DBT structures in papers. The convention for DBT diagrams is that transitions between moments are represented with labels indicating the history name and the complete group action leading to that successor. Propositions true at a moment are represented by "instance variables."
+
+There's lots of ways to write a class diagram in Mermaid, but sticking to this style is best for our poor parser. Please note that actual action names are of the form `actiontypeagentnumber`. Thus `sd1` is `Alice shoots Dan`. If we had another agent, Beth, who was represeted by 2, then `sd2` would mean `Beth shoots Dan`.
+
+If we conjoin the above fragments we can pop it into 
 """)
